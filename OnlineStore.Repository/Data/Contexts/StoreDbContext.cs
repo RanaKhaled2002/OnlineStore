5b9351a3681ce8834e359d86e156a2fa4a4353b8
RanaKhaled2002 <ranakhaeled@gmail.com>
2024-09-30 15:01:59 +0300

    Product Module - DbContext, Configyrations, Migrations

---
+++ OnlineStore.Repository/Data/Contexts/StoreDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Repository.Data.Contexts
+{
+    public  class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> Brands { get; set; }
+        public DbSet<ProductType> Types { get; set; }
+    }
+}
