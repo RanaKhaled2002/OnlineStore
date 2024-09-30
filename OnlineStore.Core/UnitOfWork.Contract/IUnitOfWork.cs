619ac03c8287c864f1b76b9b8a7574361571017e
RanaKhaled2002 <ranakhaeled@gmail.com>
2024-09-30 22:10:34 +0300

    Unit Of Work

---
+++ OnlineStore.Core/UnitOfWork.Contract/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using OnlineStore.Core.Entities;
+using OnlineStore.Core.Repositories.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Core.UnitOfWork.Contract
+{
+    public interface IUnitOfWork
+    {
+        Task<int> CompleteAsync();
+        IGenericRepository<TEntity,TKey> Repository<TEntity, TKey>() where TEntity : BaseEntity<TKey>;
+    }
+}
