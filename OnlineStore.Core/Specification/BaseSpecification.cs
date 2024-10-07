d3ecf317c5c4ea03eb94fffbd40fb4a312511efc
RanaKhaled2002 <ranakhaeled@gmail.com>
2024-10-07 14:59:36 +0300

    Prepare Specification Class

---
+++ OnlineStore.Core/Specification/BaseSpecification.cs
@@ -0,0 +1,23 @@
+﻿using OnlineStore.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Core.Specification
+{
+    public class BaseSpecification<TEntity, TKey> : ISpecification<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        public Expression<Func<TEntity, bool>> Criteria { get; set; }
+        public List<Expression<Func<TEntity, object>>> Includes { get; set; } = new List<Expression<Func<TEntity, object>>>();
+
+        public BaseSpecification() { }
+
+        public BaseSpecification(Expression<Func<TEntity, bool>> expression)
+        {
+            Criteria = expression;
+        }
+    }
+}
---
+++ OnlineStore.Core/Specification/ISpecification.cs
@@ -0,0 +1,16 @@
+﻿using OnlineStore.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Core.Specification
+{
+    public interface ISpecification<TEntity,TKey> where TEntity : BaseEntity<TKey>
+    {
+        public Expression<Func<TEntity, bool>> Criteria { get; set; }
+        public List<Expression<Func<TEntity,Object>>> Includes {  get; set; }
+    }
+}
